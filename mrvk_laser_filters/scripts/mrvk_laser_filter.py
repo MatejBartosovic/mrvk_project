61112f9a52dc1ea0c29cfc63f40f4fa232b237d5
Adam <33zuli@gmail.com>
2017-07-31 14:40:35 +0200

    Add mrvk_laser_filters (initial version)

---
+++ mrvk_laser_filters/scripts/mrvk_laser_filter.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+
+import os, sys, time, copy
+import math
+import numpy as np
+from threading import Thread
+
+import rospy
+import tf
+
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+
+class LaserFilter():
+    def __init__(self):
+        self.laser_sub = rospy.Subscriber('/scan', LaserScan, self.laser_cb)
+        self.laser_pub = rospy.Publisher('/scan_filtered', LaserScan, queue_size=10)
+        
+        # Isolated point filter
+        self.filter_isolated = rospy.get_param('~filter_isolated', True)
+        self.min_accept_size = rospy.get_param('~min_accept_size', 3)
+
+        # Transient points filter params
+        self.filter_transient = rospy.get_param('~filter_transient', True)
+        self.shift_tolerance = rospy.get_param('~shift_tolerance', 2)
+
+        self.prev_data = None
+        if self.filter_isolated:
+            rospy.loginfo("Isolated points filter enabled")
+        if self.filter_transient:
+            rospy.loginfo("Transient points filter enabled")
+
+    def laser_cb(self, data):
+        #print len(data.ranges)
+        # self.ranges_in = np.array(data.ranges)
+        # print self.ranges_in.shape
+        # self.ranges_in *= 0.5
+        
+        # if self.prev_data is not None:
+        #     print(np.array(self.prev_data.ranges))
+        #     print ""
+        debug=False
+        
+        if debug:
+            rospy.loginfo(np.array(data.ranges))
+            # print(np.array(data.ranges))
+            print ""
+        
+        if self.filter_isolated:
+            data = self.remove_isolated_points(data, self.min_accept_size)
+        if self.filter_transient:
+            data = self.remove_transient_points(data, self.shift_tolerance)
+
+        # data.ranges = self.ranges_in
+        if debug:
+            # print(data.ranges)
+            rospy.loginfo(data.ranges)
+            # print("*****************************")
+            rospy.loginfo("*****************************")
+        self.laser_pub.publish(data)
+
+    def remove_isolated_points(self, data, min_accept_size):
+        count = 0
+        is_region = False
+        i = 0
+        start_i = 0
+        end_i = 0
+        ranges = np.array(data.ranges)
+
+        for i in range(0,ranges.shape[0]):
+            dist = ranges[i]
+            if (not np.isinf(dist)) and not (np.isnan(dist)):
+                # print dist,
+                count+=1
+                if is_region == False:
+                    is_region = True
+                    start_i = i
+            else:
+                if is_region == True:
+                    is_region = False
+                    end_i = i
+                    if(count<min_accept_size):
+                        for j in range(start_i,end_i):
+                            ranges[j] = np.inf
+                    count=0
+
+            # i+=1
+        data.ranges = ranges
+        return data
+
+    def remove_transient_points(self, new_data, tolerance):
+        # convert scan to numpy array
+        new_data.ranges = np.array(new_data.ranges)
+
+        if self.prev_data is None:
+            self.prev_data = new_data
+        new_data_clean = copy.deepcopy(new_data)
+
+        # new_ranges = np.array(new_data.ranges)
+        for i in range(0, new_data.ranges.shape[0]):
+            if self.is_invalid(self.prev_data.ranges[i]) and not self.is_invalid(new_data.ranges[i]):
+                # if pixel is the same as previous
+                new_data.ranges[i] = np.inf
+            # else:
+            #     # check pixels wihtin tolerance
+            #     for s in range(0, self.shift_tolerance):
+
+            #     pass
+            # if (isnan(self.prev_data.ranges[i]) or isinf(self.prev_data.ranges[i]))
+            #     and not 
+
+        self.prev_data = new_data_clean
+        return new_data
+
+    def is_invalid(self, val):
+        return np.isinf(val) or np.isnan(val)
+
+
+def main():	
+    # print("foo")
+    # print(sys.version)
+    rospy.init_node('mrvk_laser_filter_node', anonymous=True)
+    rospy.loginfo('Starting mrvk_laser_filter node')
+    #foo
+    lf = LaserFilter()
+
+    rospy.spin()
+
+
+
+if __name__ == '__main__':
+    main()
