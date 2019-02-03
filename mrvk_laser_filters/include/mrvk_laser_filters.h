ea7a5ee025d767d2eb25282e860afa8c59e367dd
Adam Trizuljak <33zuli@gmail.com>
2019-02-03 22:49:41 +0100

    Laser: WIP common plugin

--- mrvk_laser_filters/include/mrvk_laser_filters.h
+++ mrvk_laser_filters/include/mrvk_laser_filters.h
@@ -2,26 +2,28 @@
 // Created by adam on 31.1.2019.
 //
 
-#ifndef MRVK_PROJECT_ISOLATEDPOINTFILTER_H
-#define MRVK_PROJECT_ISOLATEDPOINTFILTER_H
+#ifndef MRVK_PROJECT_MRVKLASERFILTERS_H
+#define MRVK_PROJECT_MRVKLASERFILTERS_H
 
 #include <vector>
 #include <cmath>
+
+#include <boost/circular_buffer.hpp> // TODO continue here
 
 #include <ros/ros.h>
 #include "sensor_msgs/LaserScan.h"
 
 #include "filters/filter_chain.h"
 
-namespace isolated_point_filter {
+namespace mrvk_laser_filters {
 
-class IsolatedPointFilter : public filters::FilterBase<sensor_msgs::LaserScan> {
+class MrvkLaserFilters : public filters::FilterBase<sensor_msgs::LaserScan> {
 public:
     /** \brief Constructor
      * \param averaging_length How many scans to average over.
      */
-    IsolatedPointFilter();
-    ~IsolatedPointFilter();
+    MrvkLaserFilters();
+    ~MrvkLaserFilters();
 
     bool configure();
 
@@ -31,11 +33,20 @@ public:
      */
     bool update(const sensor_msgs::LaserScan& scan_in, sensor_msgs::LaserScan& scan_out);
 
+    /**
+     *
+     * @param scan_in
+     * @param scan_out
+     * @return
+     */
+    bool updateIsolatedPointsFilter(const sensor_msgs::LaserScan& scan_in, sensor_msgs::LaserScan& scan_out);
+
 
 private:
+    bool enable_isolated_points_filter_;
     int min_accept_size_;
 };
 
 }
 
-#endif //MRVK_PROJECT_ISOLATEDPOINTFILTER_H
+#endif //MRVK_PROJECT_MRVKLASERFILTERS_H
---
+++ mrvk_laser_filters/src/mrvk_laser_filters.cpp
@@ -0,0 +1,78 @@
+//
+// Created by adam on 31.1.2019.
+//
+
+#include "mrvk_laser_filters.h"
+
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(mrvk_laser_filters::MrvkLaserFilters, filters::FilterBase<sensor_msgs::LaserScan>);
+
+namespace mrvk_laser_filters {
+
+MrvkLaserFilters::MrvkLaserFilters():
+        min_accept_size_(1), enable_isolated_points_filter_(false){
+    //
+}
+
+MrvkLaserFilters::~MrvkLaserFilters() {
+
+}
+
+bool MrvkLaserFilters::configure() {
+    ros::NodeHandle nh("~");
+    ROS_WARN("%s", nh.getNamespace().c_str());
+
+    nh.getParam("scan_filter_chain/mrvk_laser_filters/params/isolated_points_filter/enabled", enable_isolated_points_filter_);
+    nh.getParam("isolated_points_filter/min_accept_size", min_accept_size_);
+
+    ROS_WARN("IsolatedPointFilter enabled = %d, min_accept_size = %d", enable_isolated_points_filter_, min_accept_size_);
+}
+
+/** \brief Update the filter and get the response
+ * \param scan_in The new scan to filter
+ * \param scan_out The filtered scan
+ */
+bool MrvkLaserFilters::update(const sensor_msgs::LaserScan& scan_in, sensor_msgs::LaserScan& scan_out) {
+    updateIsolatedPointsFilter(scan_in, scan_out);
+    return true;
+}
+
+bool MrvkLaserFilters::updateIsolatedPointsFilter(const sensor_msgs::LaserScan& scan_in, sensor_msgs::LaserScan& scan_out) {
+    scan_out = scan_in;
+    int start_i = 0;
+    int end_i = 0;
+    int count = 0;
+    bool is_region = false;
+
+    for(int i = 0; i<scan_out.ranges.size(); i++){
+        float dist = scan_out.ranges.at(i);
+        if(!isinf(dist) && !isnan(dist)){
+            count += 1;
+            if (is_region == false) {
+                is_region = true;
+                start_i = i;
+            }
+        }
+        else {
+            if(is_region == true) {
+                is_region = false;
+                end_i = i;
+                if (count < min_accept_size_) {
+                    for(int j = start_i; i<end_i; i++) {
+                        scan_out.ranges.at(i) = INFINITY;
+                    }
+                }
+                count = 0;
+            }
+        }
+
+    }
+    return true;
+}
+
+}
+
+/*
+ * TODO:
+ * */
