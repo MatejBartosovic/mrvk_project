b8d202125e9e3b492384cc967e15dc36eed013f4
Adam Trizuljak <33zuli@gmail.com>
2019-02-04 22:14:41 +0100

    Laser: Fix IsolatedPointFilter, begin TransientPointFilter

--- mrvk_laser_filters/include/mrvk_laser_filters.h
+++ mrvk_laser_filters/include/mrvk_laser_filters.h
@@ -17,6 +17,8 @@
 
 namespace mrvk_laser_filters {
 
+#define IS_INF(val) (std::isinf(val) || std::isnan(val))
+
 class MrvkLaserFilters : public filters::FilterBase<sensor_msgs::LaserScan> {
 public:
     /** \brief Constructor
@@ -41,10 +43,31 @@ public:
      */
     bool updateIsolatedPointsFilter(const sensor_msgs::LaserScan& scan_in, sensor_msgs::LaserScan& scan_out);
 
+    /**
+     *
+     * @param scan_in
+     * @param scan_out
+     * @return
+     */
+    bool updateTransientPointsFilter(const sensor_msgs::LaserScan& scan_in, sensor_msgs::LaserScan& scan_out);
+
 
 private:
+    // Common
+    ros::Time t_last_message_;
+
+    // Isolated Points filter
     bool enable_isolated_points_filter_;
     int min_accept_size_;
+
+    // Transient Points filter
+    bool enable_transient_points_filter_;
+    int num_prev_frames_;
+    boost::circular_buffer<sensor_msgs::LaserScan>* transient_scan_buff_;
+
+    // Utils
+    void printScanHistogram(const sensor_msgs::LaserScan& scan_in);
+    void printScanDiffHistogram(const sensor_msgs::LaserScan& scan_in, const sensor_msgs::LaserScan& scan_out);
 };
 
 }
--- mrvk_laser_filters/src/mrvk_laser_filters.cpp
+++ mrvk_laser_filters/src/mrvk_laser_filters.cpp
@@ -5,28 +5,72 @@
 #include "mrvk_laser_filters.h"
 
 #include <pluginlib/class_list_macros.h>
+#include <cmath>
+#include <xmlrpcpp/XmlRpcException.h>
 
 PLUGINLIB_EXPORT_CLASS(mrvk_laser_filters::MrvkLaserFilters, filters::FilterBase<sensor_msgs::LaserScan>);
 
 namespace mrvk_laser_filters {
 
 MrvkLaserFilters::MrvkLaserFilters():
-        min_accept_size_(1), enable_isolated_points_filter_(false){
+        t_last_message_(0),
+        enable_isolated_points_filter_(false), min_accept_size_(1), \
+        enable_transient_points_filter_(false), num_prev_frames_(1), transient_scan_buff_(NULL)
+{
     //
 }
 
 MrvkLaserFilters::~MrvkLaserFilters() {
-
+    if(transient_scan_buff_)
+        delete transient_scan_buff_;
 }
 
 bool MrvkLaserFilters::configure() {
-    ros::NodeHandle nh("~");
-    ROS_WARN("%s", nh.getNamespace().c_str());
 
-    nh.getParam("scan_filter_chain/mrvk_laser_filters/params/isolated_points_filter/enabled", enable_isolated_points_filter_);
-    nh.getParam("isolated_points_filter/min_accept_size", min_accept_size_);
+    XmlRpc::XmlRpcValue ip_params, tp_params;
+    if(getParam("isolated_points_filter", ip_params)) {
+        for (auto it = ip_params.begin() ;it != ip_params.end();it++) {
+            std::cout << "param " << it->first << " has value " << ip_params[it->first]  << " type " << ip_params[it->first].getType() << std::endl;
+        }
+        try {
+            if (ip_params.hasMember("enabled")) {
+                enable_isolated_points_filter_ = ip_params["enabled"];
+                min_accept_size_ = ip_params["min_accept_size"];
+            }
+        }
+        catch (XmlRpc::XmlRpcException exc) {
+            ROS_ERROR("IsolatedPointsFilter wrong or missing parameters!");
+            std::cout << exc.getMessage() << std::endl;
+        }
+    }
+
+    if(getParam("transient_points_filter", tp_params)) {
+        for (auto it = tp_params.begin() ;it != tp_params.end();it++) {
+            std::cout << "param " << it->first << " has value " << ip_params[it->first]  << " type " << ip_params[it->first].getType() << std::endl;
+        }
+        try {
+            if (tp_params.hasMember("enabled")) {
+                enable_transient_points_filter_ = tp_params["enabled"];
+                num_prev_frames_ = tp_params["num_prev_frames"];
+            }
+        }
+        catch (XmlRpc::XmlRpcException exc) {
+            ROS_ERROR("TransientPointsFilter wrong or missing parameters!");
+            std::cout << exc.getMessage() << std::endl;
+        }
+
+        transient_scan_buff_ = new boost::circular_buffer<sensor_msgs::LaserScan>(num_prev_frames_);
+    }
+
+//    auto it = ip_params.find
+//    ros::NodeHandle nh("~");
+//    ROS_WARN("%s", nh.getNamespace().c_str());
+//
+//    nh.getParam("scan_filter_chain/mrvk_laser_filters/ip_params/isolated_points_filter/enabled", enable_isolated_points_filter_);
+//    nh.getParam("isolated_points_filter/min_accept_size", min_accept_size_);
 
     ROS_WARN("IsolatedPointFilter enabled = %d, min_accept_size = %d", enable_isolated_points_filter_, min_accept_size_);
+    ROS_WARN("TransientPointFilter enabled = %d, num_prev_frames = %d", enable_transient_points_filter_, num_prev_frames_);
 }
 
 /** \brief Update the filter and get the response
@@ -34,7 +78,21 @@ bool MrvkLaserFilters::configure() {
  * \param scan_out The filtered scan
  */
 bool MrvkLaserFilters::update(const sensor_msgs::LaserScan& scan_in, sensor_msgs::LaserScan& scan_out) {
-    updateIsolatedPointsFilter(scan_in, scan_out);
+//    printScanHistogram(scan_in);
+
+    if(enable_isolated_points_filter_) {
+        updateIsolatedPointsFilter(scan_in, scan_out);
+    }
+
+    if(enable_transient_points_filter_) {
+        updateTransientPointsFilter(scan_in, scan_out);
+    }
+
+//    printScanHistogram(scan_out);
+//    std::cout << std::endl;
+    printScanDiffHistogram(scan_in, scan_out);
+    t_last_message_ = scan_in.header.stamp;
+
     return true;
 }
 
@@ -47,20 +105,20 @@ bool MrvkLaserFilters::updateIsolatedPointsFilter(const sensor_msgs::LaserScan& 
 
     for(int i = 0; i<scan_out.ranges.size(); i++){
         float dist = scan_out.ranges.at(i);
-        if(!isinf(dist) && !isnan(dist)){
+        if(!std::isinf(dist) && !std::isnan(dist)){
             count += 1;
-            if (is_region == false) {
+            if (!is_region) {
                 is_region = true;
                 start_i = i;
             }
         }
         else {
-            if(is_region == true) {
+            if(is_region) {
                 is_region = false;
                 end_i = i;
                 if (count < min_accept_size_) {
-                    for(int j = start_i; i<end_i; i++) {
-                        scan_out.ranges.at(i) = INFINITY;
+                    for(int j = start_i; j<end_i; j++) {
+                        scan_out.ranges.at(j) = INFINITY;
                     }
                 }
                 count = 0;
@@ -69,6 +127,43 @@ bool MrvkLaserFilters::updateIsolatedPointsFilter(const sensor_msgs::LaserScan& 
 
     }
     return true;
+}
+
+bool MrvkLaserFilters::updateTransientPointsFilter(const sensor_msgs::LaserScan& scan_in, sensor_msgs::LaserScan& scan_out) {
+    scan_out = scan_in;
+    transient_scan_buff_->push_back(scan_in);
+}
+
+void MrvkLaserFilters::printScanHistogram(const sensor_msgs::LaserScan& scan_in) {
+    std::cout << scan_in.header.stamp << " ";
+    for(int i = 0; i<scan_in.ranges.size(); i++){
+        float dist = scan_in.ranges.at(i);
+        if(std::isinf(dist) || std::isnan(dist)){
+            std::cout << "_";
+        }
+        else {
+            std::cout << "#";
+        }
+    }
+    std::cout << std::endl;
+}
+
+void MrvkLaserFilters::printScanDiffHistogram(const sensor_msgs::LaserScan& scan_in, const sensor_msgs::LaserScan& scan_out) {
+    std::cout << scan_in.header.stamp << " ";
+    for(int i = 0; i<scan_in.ranges.size(); i++){
+        float dist_in = scan_in.ranges.at(i);
+        float dist_out = scan_out.ranges.at(i);
+        if(!IS_INF(dist_in) && IS_INF(dist_out)) {
+            std::cout << "x";
+        }
+        else if(IS_INF(dist_in)){
+            std::cout << "_";
+        }
+        else {
+            std::cout << "#";
+        }
+    }
+    std::cout << std::endl;
 }
 
 }
